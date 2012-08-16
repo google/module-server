11a4132a99f41ef93527ed6647f611a5811ca829
Malte Ubl <malte.ubl@gmail.com>
2012-08-15 19:56:19 -0700

    Tests for source maps

--- test/test-module-server.js
+++ test/test-module-server.js
@@ -4,13 +4,14 @@ var server = require('../module-server').from('./fixtures/build',
     './fixtures/graph.json');
 
 test('module server', function(t) {
-  t.plan(10);
+  t.plan(11);
 
   var subApp = getJs('module$sub_app');
-  server(['module$sub_app'], [], function(err, length, js) {
+  server(['module$sub_app'], [], function(err, length, js, sourceMap) {
     t.is(err, null);
     t.is(js, subApp);
     t.is(length, subApp.length);
+    t.is(sourceMap, null);
   });
 
   var expectedJs = subApp + getJs(['module$module$bar',
@@ -52,7 +53,47 @@ test('logging', function(t) {
       t.pass()
     }
   });
-})
+});
+
+test('source maps', function(t) {
+  t.plan(19);
+
+  var prefix = 'http://prefix';
+
+  var options = {
+    createSourceMap: true,
+    sourceMapSourceRootUrlPrefix: prefix
+  };
+
+  var files = ['module$sub_app', 'module$module$bar', 'module$module$foo'];
+
+  var expectedJs = getJs(files);
+  server(['module$sub_app', 'module$module$foo'], ['module$module$baz$foo'],
+      function(err, length, js, map) {
+    t.is(err, null);
+    t.is(js, expectedJs);
+    t.is(length, expectedJs.length);
+    t.ok(map != null);
+    t.is(map.version, 3);
+    t.ok(typeof map.file === 'string'); // we don't care about the value;
+    var sections = map.sections;
+    t.ok(sections != null);
+    t.ok(sections.length, 3);
+    var s0 = sections[0];
+    t.is(s0.offset.line, 1);
+    t.is(s0.offset.column, 0);
+    t.ok(s0.map);
+    t.ok(s0.map.version, 3);
+    t.ok(s0.map.file);
+    t.ok(s0.map.mappings);
+    t.ok(s0.map.sourceRoot, prefix);
+
+    t.is(sections[1].offset.line, 3);
+    t.is(sections[1].offset.column, 0);
+    t.is(sections[2].offset.line, 5);
+    t.is(sections[2].offset.column, 0);
+  }, options);
+});
 
 function getJs(names) {
   if (typeof names === 'string') {
